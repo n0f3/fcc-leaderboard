e550efcb430c16841799dd9c284ace6e811b6e71
n0f3 <n0f3@outlook.com>
2017-03-30 23:26:08 -0700

    Changed app to stateless component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Leaderboard from './Leaderboard';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="panel panel-default">
-        <div className="panel-heading text-center" >
-          <h1>FreeCodeCamp Browny Points Leaderboard</h1>
-        </div>
-        <Leaderboard/>
-        </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading text-center" >
+        <h1>FreeCodeCamp Browny Points Leaderboard</h1>
+      </div>
+      <Leaderboard/>
+    </div>
+  );
 }
 
 export default App;
